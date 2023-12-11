d46cf8b5f7428c6909dae311050f902d2c2f6673
SellierL <152199622+SellierL@users.noreply.github.com>
2023-12-11 17:58:07 +0100

    Update 2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,25 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_BoutonArret_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Arrêt de l'application");
+    msgBox.setInformativeText("Voulez-vous vraiment arrêter l'application ?");
+    msgBox.setStandardButtons(QMessageBox::Yes);
+    int ret = msgBox.exec();
+    // On vérifie si l'utilisateur a cliqué sur Yes
+
+    if ( ret == QMessageBox::Yes )
+    {
+    // traitement à exécuter pour arrêter l'application et fermer toutes les fenetres
+    }
+    else
+    {
+       //Fermer juste la fenetre msgBox
+    }
 }
 
